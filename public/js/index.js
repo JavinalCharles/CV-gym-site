938b50a7b89269fe0d01b04da78209a0de59c6e6
JavinalCharles <javinal.charlemagne@gmail.com>
2022-04-17 13:23:25 +0800

    Minor change. Prevent contextmenu for .class-item

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$('.classes-item').bind('contextmenu', function(e) {
 		e.preventDefault();
 	});
+	$('.team-item').bind('contextmenu', function(e) {
+		e.preventDefault();
+	});
 });
